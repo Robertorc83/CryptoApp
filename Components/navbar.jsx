c3e7b569646555f5d831477f1ca6a5535783c374
Robertorc83 <50055949+Robertorc83@users.noreply.github.com>
2022-07-17 10:39:02 -0500

    configuration for aws amplify

--- Components/navbar.jsx
+++ Components/navbar.jsx
@@ -6,7 +6,7 @@ function Navbar({linkStyles, Logo}) {
   return (
     <div className='absolute md:w-[93%] xl:w-[95%]'>
         <div className=' mt-5 ml-5 w-[50px] md:w-[60px] lg:w-[100px]'>
-            <Link href="/"><Image src={Logo}/></Link>
+            <Link href="/"><Image src={Logo} alt="logo crynnov"/></Link>
         </div>
         <div className='hidden md:flex mt-[-40px] flex-row justify-end '>
             <div>
@@ -22,19 +22,19 @@ function Navbar({linkStyles, Logo}) {
             <Link href="/contact"><a className={linkStyles}>Contact</a></Link>
             </div>
         </div>
-        <div class="flex absolute w-screen mt-[-35px] justify-end pr-[20px] lg:hidden">
+        <div className="flex absolute w-screen mt-[-35px] justify-end pr-[20px] lg:hidden">
           <button id="dropdownBottomButton" data-dropdown-toggle="dropdownBottom" data-dropdown-placement="bottom" className='inline-flex items-center ' type="button">
             <svg
             xmlns="http://www.w3.org/2000/svg"
-            class="w-6 h-6"
+            className="w-6 h-6"
             fill="none"
             viewBox="0 0 24 24"
             stroke="#3f0357"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="M4 6h16M4 12h16M4 18h16"
             />
           </svg> 
